f79946f90344bd4d3b08c6741fe6806796fabfa0
togashi <KGG03575@nifty.com>
2021-08-20 09:14:22 +0900

    change shinkansen_simulator to shinkansen-simulator

--- shinkansen-simulator/asgi.py
+++ shinkansen-simulator/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for shinkansen_simulator project.
+ASGI config for shinkansen-simulator project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shinkansen_simulator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shinkansen-simulator.settings')
 
 application = get_asgi_application()
--- shinkansen-simulator/urls.py
+++ shinkansen-simulator/urls.py
@@ -1,4 +1,4 @@
-"""shinkansen_simulator URL Configuration
+"""shinkansen-simulator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
