0e80d669f91efaafeddea8547c1ec20d0fb9b434
Nobukazu Togashi <KGG03575@nifty.com>
2021-11-01 04:47:47 +0900

    delete SECRET_KEY from settings.py

--- shinkansen_simulator/settings.py
+++ shinkansen_simulator/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vf4u8j1wj*egmj_w6b%^0bnl8_xm+3xa=hz)tkvxczuoet^w$r'
+# SECRET_KEY = 'vf4u8j1wj*egmj_w6b%^0bnl8_xm+3xa=hz)tkvxczuoet^w$r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,3 +122,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shinkansen_simulator', 'static')]
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    SECRET_KEY = os.environ['SECRET_KEY']
+    # import django_heroku
+    # django_heroku.settings(locals())
+
